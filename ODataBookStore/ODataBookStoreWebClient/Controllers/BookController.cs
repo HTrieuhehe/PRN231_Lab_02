9efe58ca5919e55e247b695fd87cff166c7d2cc6
HTrieuhehe <97211030+HTrieuhehe@users.noreply.github.com>
2023-06-28 19:10:34 +0700

    Add: Gắn link API vào Login (Có lỗi 500)

--- ODataBookStore/ODataBookStoreWebClient/Controllers/BookController.cs
+++ ODataBookStore/ODataBookStoreWebClient/Controllers/BookController.cs
@@ -10,21 +10,15 @@ namespace ODataBookStoreWebClient.Controllers
     public class BookController : Controller
     {
         private readonly HttpClient client = null;
-        private string ProductApiUrl = "";
-        private string AccountLoginApiUrl = "";
-        private string UserLoginApiUrl = "";
+        private readonly string ProductApiUrl = "";
 
         public BookController()
         {
             client = new HttpClient();
             var contentType = new MediaTypeWithQualityHeaderValue("application/json");
             client.DefaultRequestHeaders.Accept.Add(contentType);
+
             ProductApiUrl = "https://localhost:7057/odata/Book";
-
-            /* https://localhost:44319/Account/Login?username=string&password=string */
-
-            AccountLoginApiUrl = "https://localhost:44319/Account/Login";
-            UserLoginApiUrl = "https://localhost:44319/User/Login";
         }
           
         public async Task<IActionResult> Index()
@@ -45,7 +39,7 @@ namespace ODataBookStoreWebClient.Controllers
         }
         public async Task<IActionResult> Details(int id)
         {
-            HttpResponseMessage response = await client.GetAsync(AccountLoginApiUrl + $"/{id}?");
+            HttpResponseMessage response = await client.GetAsync(ProductApiUrl + $"/{id}?$expand=Location,Press");
             string strData = await response.Content.ReadAsStringAsync();
             dynamic temp = JObject.Parse(strData);
             Book book = new Book
@@ -73,15 +67,7 @@ namespace ODataBookStoreWebClient.Controllers
                
         }
 
-        public async Task<IActionResult> AccountLogin(string username, string password)
-        {
-            HttpResponseMessage response = await client.GetAsync(ProductApiUrl + $"/{id}?$expand=Location,Press");
-            string strData = await response.Content.ReadAsStringAsync();
-            dynamic temp = JObject.Parse(strData);
-
-            return View(temp);
-        }
-
+        
 
         //public IActionResult Create()
         //{
--- ODataBookStore/ODataBookStoreWebClient/Controllers/LoginController.cs
+++ ODataBookStore/ODataBookStoreWebClient/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using ODataBookStore.Models;
 using System.Net.Http.Headers;
 
 namespace ODataBookStoreWebClient.Controllers
@@ -6,14 +8,25 @@ namespace ODataBookStoreWebClient.Controllers
     public class LoginController : Controller
     {
         private readonly HttpClient client = null;
-        private string LoginApiUrl = "";
+
+        private readonly string AccountLoginApiUrl = "";
+        private readonly string AccountRegisterApiUrl = "";
+
+        private readonly string UserLoginApiUrl = "";
+        private readonly string UserRegisterApiUrl = "";
 
         public LoginController()
         {
             client = new HttpClient();
             var contentType = new MediaTypeWithQualityHeaderValue("application/json");
             client.DefaultRequestHeaders.Accept.Add(contentType);
-            LoginApiUrl = "";
+
+
+            AccountLoginApiUrl = "https://localhost:44319/Account/Login?";
+            AccountRegisterApiUrl = "https://localhost:44319/Account/Register?";
+
+            UserLoginApiUrl = "https://localhost:44319/User/Login?";
+            UserRegisterApiUrl = "https://localhost:44319/User/Register?";
         }
 
         public async Task<IActionResult> Index()
@@ -21,14 +34,36 @@ namespace ODataBookStoreWebClient.Controllers
             return View("~/Views/Login/Index.cshtml");
         }
 
-        //public async Task<IActionResult> AdminLogin()
-        //{
-
-        //}
-
-        public async Task<IActionResult> Login(string username, string password)
+        public async Task<IActionResult> AccountLogin(string username, string password)
         {
-            return BadRequest();
+            HttpResponseMessage response = await client.GetAsync(AccountLoginApiUrl + $"username={username}&password={password}");
+            string strData = await response.Content.ReadAsStringAsync();
+            dynamic temp = JObject.Parse(strData);
+
+            if (temp != null)
+            {
+                TempData["UserData"] = temp;
+
+                return RedirectToAction("../Book/Index", "Book");
+            }
+            return View("../Login/Index");
         }
+
+        public async Task<IActionResult> AccountRegister(string username, string password)
+        {
+            HttpResponseMessage response = await client.GetAsync(AccountRegisterApiUrl + $"username={username}&password={password}");
+            string strData = await response.Content.ReadAsStringAsync();
+            dynamic temp = JObject.Parse(strData);
+
+            Account account = new Account()
+            {
+                Id = temp.Id,
+                RoleId = temp.RoleId,
+                Username = temp.Username,
+            };
+            return View(account);
+
+        }
+
     }
 }
